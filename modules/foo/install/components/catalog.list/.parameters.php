c9401ac7b9af1349e568906768199090aa35e10f
jigius <jigius@gmail.com>
2023-07-13 16:53:08 +0700

    fixes parameters

--- modules/foo/install/components/catalog.list/.parameters.php
+++ modules/foo/install/components/catalog.list/.parameters.php
@@ -3,34 +3,30 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     throw new LogicException("Environment is invalid");
 }
-$arSortFields = [
-    "ID" => GetMessage("FOO_CATALOG_LIST_DESC_FID"),
-    "NAME" => GetMessage("FOO_CATALOG_LIST_DESC_FNAME"),
-    "CREATED" => GetMessage("FOO_CATALOG_LIST_DESC_FCREATED"),
-];
-$arSorts = [
-    "ASC" => GetMessage("FOO_CATALOG_LIST_DESC_ASC"),
-    "DESC" => GetMessage("FOO_CATALOG_LIST_DESC_DESC"),
-];
 
 $arComponentParameters = [
     "GROUPS" => [],
     "PARAMETERS" => [
-        "SORT_BY" => [
+        "BRAND_ID" => [
             "PARENT" => "DATA_SOURCE",
-            "NAME" => GetMessage("FOO_CATALOG_LIST_DESC_ORDER"),
-            "TYPE" => "LIST",
-            "DEFAULT" => "ID",
-            "VALUES" => $arSortFields,
-            "ADDITIONAL_VALUES" => "N",
+            "NAME" => "BRAND_ID",
+            "TYPE" => "STRING",
+            "DEFAULT" => "",
+            "ADDITIONAL_VALUES" => "N"
         ],
-        "SORT_ORDER" => [
+        "MODEL_ID" => [
             "PARENT" => "DATA_SOURCE",
-            "NAME" => GetMessage("FOO_CATALOG_LIST_DESC_BY"),
-            "TYPE" => "LIST",
-            "DEFAULT" => "DESC",
-            "VALUES" => $arSorts,
-            "ADDITIONAL_VALUES" => "N",
+            "NAME" => "MODEL_ID",
+            "TYPE" => "STRING",
+            "DEFAULT" => "",
+            "ADDITIONAL_VALUES" => "N"
+        ],
+        "DETAIL_ID" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => "DETAIL_ID",
+            "TYPE" => "STRING",
+            "DEFAULT" => "",
+            "ADDITIONAL_VALUES" => "N"
         ]
     ],
 ];
--- modules/foo/install/components/catalog.list/lang/ru/.parameters.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['FOO_CATALOG_LIST_DESC_FID'] = "ИД";
-$MESS['FOO_CATALOG_LIST_DESC_FNAME'] = "Наименование";
-$MESS['FOO_CATALOG_LIST_DESC_FCREATED'] = "Создан";
